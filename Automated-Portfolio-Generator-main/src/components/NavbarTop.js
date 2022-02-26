d56bd1b80b80107b970c49468ee37c951d8e4198
Anthima Singh <58143009+Anthima@users.noreply.github.com>
2022-02-26 22:07:37 +0530

    Logo name change

--- Automated-Portfolio-Generator-main/src/components/NavbarTop.js
+++ Automated-Portfolio-Generator-main/src/components/NavbarTop.js
@@ -46,7 +46,7 @@ function NavbarTop({ goBack, switchComp }) {
             fontSize="large"
             className={classes.icon} /> */}
             <div>
-              headportfoliogen
+              PortfolioGen
             </div>
 
           {switchComp}
